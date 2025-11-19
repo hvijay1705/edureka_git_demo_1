822971ef88bf342bf9141f91de28ad556e26378f
Himanshu Vijay <learning.hvijay1705@gmail.com>
2025-11-19 20:23:21 +0000

    Edureka-Demo1-Commit-v11

---
+++ firstcode.py
@@ -0,0 +1,15 @@
+import datetime
+import random
+
+def greet_user(name):
+    now = datetime.datetime.now()
+    number = random.randint(1, 100)
+    print(f"Hello, {name}! Welcome to Git Demo - Fristcode. Your ID number is: {number} and your login time in : {now}")
+
+def main():
+    
+    user_name = input("Enter your first & last name: ")
+    greet_user(user_name)
+
+if __name__ == "__main__":
+    main()
